44a1a7b88b7b95e7e5452a3ef1a0d1a8a3338a86
smabari <mdazizulbarishafin@gmail.com>
2025-09-12 06:09:30 +0600

    starting out

---
+++ 2.calculationOrder.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	// double > float > long long int > long int > int > characters
+	double a = 3/2;
+	// calculation is being first than it's being stored in the duble
+	cout << a;
+	return 0;
+}
---
+++ 3.overflow.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	// int, char, long int, long long int, float, double
+
+	// -10^9 < int < 10^9
+	// -10^12 < long int < 10^12
+    // -10^18 < long long int < 10^18
+
+    int a = 100000;
+    int b = 100000;
+    int c = a*b;
+    cout << c << endl;
+	
+}
